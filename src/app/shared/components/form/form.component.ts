7a6912e08ca15703f976288898c70e45bcf62071
Uladzislau Sakalou1 <uladzislau_sakalou1@epam.com>
2019-04-03 10:00:45 +0300

    feat: add form component and style

--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -1,14 +1,48 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
+
+import { Student, Subject } from "../../../../common/entities";
+
+import { StudentsComponent } from "../../../components/students/students.component";
+import { SubjectsComponent } from "../../../components/subjects/subjects.component";
 
 @Component({
+  providers: [StudentsComponent, SubjectsComponent],
   selector: "app-form",
   templateUrl: "./form.component.html",
   styleUrls: ["./form.component.scss"]
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  @Input() public info: any;
 
-  public ngOnInit() { }
+  public profileForm: any = new FormGroup({
+    name: new FormControl(""),
+    lastName: new FormControl(""),
+    teacher: new FormControl(""),
+    address: new FormControl(""),
+    cabinet: new FormControl(""),
+    description: new FormControl("")
+  });
+
+  constructor(
+    private studentsComponent: StudentsComponent,
+    private subjectsComponent: SubjectsComponent
+  ) { }
+
+  public ngOnInit(): any {}
+
+  public onSubmit(type: string): void {
+    let name: string = this.profileForm.value.name;
+    let lastName: string = this.profileForm.value.lastName;
+    let address: string = this.profileForm.value.address;
+    let description: string = this.profileForm.value.description;
+    let teacher: string = this.profileForm.value.teacher;
+    let cabinet: number = this.profileForm.value.cabinet;
+
+    type === "student" ?
+      this.studentsComponent.addStudent({name, lastName, address, description} as Student) :
+      this.subjectsComponent.addSubject({name, teacher, cabinet, description} as Subject);
+  }
 
 }
