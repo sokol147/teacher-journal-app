e7282b85f79eba39d9e81f3701780378b6d87ac1
Uladzislau Sakalou1 <uladzislau_sakalou1@epam.com>
2019-04-03 10:06:29 +0300

    refac: create arr with shared components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { ButtonComponent } from "../shared/components/button/button.component";
 import { FormComponent } from "../shared/components/form/form.component";
 
+let sharedComponents = [ButtonComponent, FormComponent]
+
 @NgModule({
-  imports: [ CommonModule ],
-  declarations: [ ButtonComponent, FormComponent ],
+  imports: [ CommonModule, ReactiveFormsModule ],
+  declarations: [ ...sharedComponents ],
   exports: [
-    ButtonComponent, FormComponent, CommonModule
+    ...sharedComponents, CommonModule
   ]
 })
 
