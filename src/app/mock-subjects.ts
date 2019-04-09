037b0e216613c377e6d75bfbda24d593f18b2cf3
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-09 16:29:19 +0300

    refac: change mock-data

--- src/app/mock-subjects.ts
+++ src/app/mock-subjects.ts
@@ -1,21 +1,127 @@
 import { Subject } from "./common/entities";
 
 export const SUBJECTS: Subject[] = [
-  { id: 1,
-    name: "Math",
-    teacher: "Mr.Plus",
-    cabinet: 5,
-    description: "Know Math",
-
-  },
-  { id: 2,
+  {
+    id: 1,
     name: "Biology",
-    teacher: "Ms.Dar",
-    cabinet: 3, description: "",
-
+    teacher: "Ms. Poison",
+    cabinet: 10,
+    description: "Know evolution theory",
+    date: [ "" ],
+    students: [
+      {
+        name: "Max",
+        lastName: "Pain",
+        averageMark: null,
+        marks: [
+          {
+            day: "",
+            mark: ""
+          }
+        ]
+      }, {
+        name: "Luck",
+        lastName: "Osm",
+        averageMark: null,
+        marks: [
+          {
+            day: "",
+            mark: ""
+          }
+        ]
+      }, {
+        name: "Julia",
+        lastName: "Fix",
+        averageMark: null,
+        marks: [
+          {
+            day: "",
+            mark: ""
+          }
+        ]
+      }
+    ]
   },
-  { id: 3,
-    name: "Eng",
-    teacher: "Mr.Johan",
-  }
-];
+  {
+    id: 2,
+    name: "Maths",
+    teacher: "Mr. Plus",
+    cabinet: 14,
+    description: "",
+    date: [ "" ],
+    students: [
+      {
+        name: "Max",
+        lastName: "Pain",
+        averageMark: null,
+        marks: [
+          {
+            day: "",
+            mark: ""
+          }
+        ]
+      }, {
+        name: "Luck",
+        lastName: "Osm",
+        averageMark: null,
+        marks: [
+          {
+            day: "",
+            mark: ""
+          }
+        ]
+      }, {
+        name: "Julia",
+        lastName: "Fix",
+        averageMark: null,
+        marks: [
+          {
+            day: "",
+            mark: ""
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: "History",
+    teacher: "Mr. Napoleon",
+    cabinet: 10,
+    description: "Hate russian winter",
+    date: [ "" ],
+    students: [
+      {
+        name: "Max",
+        lastName: "Pain",
+        averageMark: null,
+        marks: [
+          {
+            day: "",
+            mark: ""
+          }
+        ]
+      }, {
+        name: "Luck",
+        lastName: "Osm",
+        averageMark: null,
+        marks: [
+          {
+            day: "",
+            mark: ""
+          }
+        ]
+      }, {
+        name: "Julia",
+        lastName: "Fix",
+        averageMark: null,
+        marks: [
+          {
+            day: "",
+            mark: ""
+          }
+        ]
+      }
+    ]
+  },
+]
