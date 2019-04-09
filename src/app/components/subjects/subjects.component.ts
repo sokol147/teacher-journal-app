c908a6a6062c544597c7b2f1cf22b72d7e25abbf
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-09 16:27:57 +0300

    feat: add redux to subjects component

--- src/app/components/subjects/subjects.component.ts
+++ src/app/components/subjects/subjects.component.ts
@@ -3,12 +3,18 @@ import { Subject, Button, Student } from "src/app/common/entities";
 
 import { DbService } from "../../common/services/db.service";
 
+import { NgRedux, select } from '@angular-redux/store';
+import { IAppState } from '../../store'
+import { ADD_SUBJECT } from '../../actions';
+
 @Component({
   selector: "app-subjects",
   templateUrl: "./subjects.component.html",
   styleUrls: ["./subjects.component.scss"]
 })
 export class SubjectsComponent implements OnInit {
+
+  @select() Subjects;
 
   private formInfo: object = {
     title: "Add new Subject",
@@ -25,30 +31,34 @@ export class SubjectsComponent implements OnInit {
     class: "btn--add"
   };
 
-  public subjects: Subject[];
-  public studentsList: Student[];
+  // public subjects: Subject[];
+  // public studentsList: Student[];
 
   constructor(
-    private dbService: DbService
+    private dbService: DbService,
+    private ngRedux: NgRedux<IAppState>
   ) { }
 
   public ngOnInit(): void {
-    this.getSubjects();
-    this.getStudents();
+    // this.getSubjects();
+    // this.getStudents();
   }
 
-  public getSubjects(): void {
-    this.dbService.getSubjects()
-      .subscribe(subjects => this.subjects = subjects);
-  }
+  // public getSubjects(): void {
+  //   this.dbService.getSubjects()
+  //     .subscribe(subjects => this.subjects = subjects);
+  // }
 
-  public getStudents(): void {
-    this.dbService.getStudents()
-      .subscribe(students => this.studentsList = students);
-  }
+  // public getStudents(): void {
+  //   this.dbService.getStudents()
+  //     .subscribe(students => this.studentsList = students);
+  // }
 
   public addSubject(subject: Subject): void {
-    this.dbService.addSubject(subject)
-      .subscribe(subjects => this.subjects = subjects);
+
+    this.ngRedux.dispatch({type: ADD_SUBJECT, subject: subject});
+
+    // this.dbService.addSubject(subject)
+    //   .subscribe(subjects => this.subjects = subjects);
   }
 }
