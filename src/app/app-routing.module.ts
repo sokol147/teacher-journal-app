eeec136c2c525b7d0df3dd3dddb0ffa0d611455c
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-18 10:42:10 +0300

    feat: add statistic component to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { StudentsComponent } from "./components/students/students.component";
 import { SubjectsComponent } from "./components/subjects/subjects.component";
 import { SubjectTableComponent } from "./components/subject-table/subject-table.component";
+import { StatisticComponent } from "./components/statistic/statistic.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/students", pathMatch: "full"},
   { path: "students", component: StudentsComponent },
   { path: "subjects", component: SubjectsComponent },
-  { path: "subject/:name", component: SubjectTableComponent }
+  { path: "subject/:name", component: SubjectTableComponent },
+  { path: "statistic", component: StatisticComponent }
 ];
 
 @NgModule({
