57e659d798ef2293788ecfb2e81a5542a0ac6057
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-18 10:49:47 +0300

    feat: add save to localStorage and get current day function

--- src/app/components/subject-table/subject-table.component.ts
+++ src/app/components/subject-table/subject-table.component.ts
@@ -6,6 +6,7 @@ import { ButtonType, Button } from "../../shared/components/button/button.model"
 
 import { Observable } from "rxjs";
 import { Store } from "@ngrx/store";
+import { SubjectService } from "src/app/common/services/subjects.service";
 
 @Component({
   selector: "app-subject-table",
@@ -25,9 +26,12 @@ export class SubjectTableComponent implements OnInit {
   };
   public subject$: Observable<Subject>;
 
+  public currentDate: string;
+
   constructor(
     private _store: Store<any>,
     private route: ActivatedRoute,
+    private subjectService: SubjectService
   ) {
     const name: string = this.route.snapshot.paramMap.get("name");
     this.subject$ = _store.select(state => {
@@ -39,11 +43,12 @@ export class SubjectTableComponent implements OnInit {
   public ngOnInit(): void {}
 
   public addDay(): void {
-    this.subject.date.push("");
+    this.subjectService.getCurrentDate()
+      .subscribe(date => this.currentDate = date);
+    this.subject.date.push(this.currentDate);
     this.subject.students.forEach(student => {
-      student.marks.push({ day: "", mark: "" });
+      student.marks.push({ day: this.currentDate, mark: "" });
     });
-    console.log(this.subject);
   }
 
   public calcAverage(): void {
@@ -61,5 +66,12 @@ export class SubjectTableComponent implements OnInit {
     });
   }
 
-  public saveSubject(): void {}
+  public saveSubject(): void {
+    let currentSubj: string = this.subject.name;
+    let localSubjects: Subject[] = JSON.parse(localStorage.getItem("subjects"));
+    for (let i: number = 0; i < localSubjects.length; i++) {
+      if (localSubjects[i].name === currentSubj) { localSubjects[i] = this.subject; }
+    }
+    localStorage.setItem("subjects", JSON.stringify(localSubjects));
+  }
 }
