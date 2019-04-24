0f4b7c3da73e2838df761deb6619e5b7dac91721
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-24 15:02:34 +0300

    feat: add notifications to project

--- src/app/root/app.component.ts
+++ src/app/root/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, ViewContainerRef, ComponentFactoryResolver, ComponentRef, ComponentFactory } from "@angular/core";
+
+import { MessageComponent } from '../components/message/message.component';
 
 import { TranslateService } from "@ngx-translate/core";
 
@@ -9,9 +11,17 @@ import { TranslateService } from "@ngx-translate/core";
 })
 export class AppComponent {
 
+  @ViewChild('messagecontainer', {read: ViewContainerRef}) entry: ViewContainerRef;
+
+
   public title: string = "Teacher Journal";
 
-  constructor(private translate: TranslateService) {
+  componentRef: any;
+
+  constructor(
+    private translate: TranslateService,
+    private resolver: ComponentFactoryResolver
+  ) {
     translate.setDefaultLang("en");
   }
 
@@ -19,4 +29,18 @@ export class AppComponent {
     this.translate.use(language);
   }
 
+  createComponent(message, type) {
+    this.entry.clear();
+    const factory = this.resolver.resolveComponentFactory(MessageComponent);
+    this.componentRef = this.entry.createComponent(factory);
+    this.componentRef.instance.message = message;
+    this.componentRef.instance.type = type;
+    setTimeout(() => {
+      this.destroyComponent()
+    }, 5000)
+  }
+
+  destroyComponent() {
+    this.componentRef.destroy();
+  }
 }
