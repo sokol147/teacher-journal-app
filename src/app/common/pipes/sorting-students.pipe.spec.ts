ad61a9f39c4b8c7d34fcc46cbcbe59df5277ad73
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-26 18:06:55 +0300

    feat: add test for sorting student pipe

---
+++ src/app/common/pipes/sorting-students.pipe.spec.ts
@@ -0,0 +1,14 @@
+import { SortingStudentsPipe } from './sorting-students.pipe';
+import { STUDENTS } from 'src/app/mock-students';
+
+describe('SortingStudentPipe', () => {
+  let pipe = new SortingStudentsPipe();
+
+  it('should return Students sorted by id', () => {
+    expect(pipe.transform(STUDENTS, ["id"], 1)).toBe(STUDENTS)
+  })
+
+  it('should return Students sorted by id from last to first', () => {
+    expect(pipe.transform(STUDENTS, ["id"], -1)).toBe(STUDENTS.reverse())
+  })
+})
