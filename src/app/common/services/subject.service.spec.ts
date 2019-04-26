4ccc6668defcd22902440bbc6e2b31208ca8dfe8
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-26 18:08:32 +0300

    feat: add tests for subject service

---
+++ src/app/common/services/subject.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SubjectService } from './subjects.service';
+
+describe('SubjectService', () => {
+  let service: SubjectService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [SubjectService]
+    })
+
+    service = TestBed.get(SubjectService)
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+
+});
