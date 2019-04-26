cd4c4aa9980652a99e836cf1f152879546f6b850
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-26 18:27:38 +0300

    feat: add test for form component

---
+++ src/app/shared/components/form/form.component.spec.ts
@@ -0,0 +1,20 @@
+import { FormComponent } from './form.component';
+
+describe('FormComponent', () => {
+
+  let value = {
+    address: "",
+    cabinet: "",
+    description: "",
+    lastName: "",
+    name: "",
+    teacher: ""
+  }
+  
+  it('raise the submit event when form submited', () => {
+    const comp = new FormComponent();
+    
+    comp.submitted.subscribe(d => expect(d).toBe(value));
+    comp.onSubmit(value)
+  })
+})
