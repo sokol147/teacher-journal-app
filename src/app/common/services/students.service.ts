e8a4e419d4658d0b1e0e8504cf8b31381f07f43e
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-26 18:33:52 +0300

    refac: remove logic to service

--- src/app/common/services/students.service.ts
+++ src/app/common/services/students.service.ts
@@ -3,20 +3,33 @@ import { Observable, of } from "rxjs";
 
 import { IStudent } from "../entities";
 import { STUDENTS } from "../../mock-students";
+import { IAppState } from 'src/app/store/state/app.state';
+import { Store, select } from '@ngrx/store';
+import { AddStudent } from 'src/app/store/actions/app.actions';
+import { AppComponent } from 'src/app/root/app.component';
+import { selectStudentList } from 'src/app/store/selectors/student.selector';
 
 @Injectable({
   providedIn: "root"
 })
 export class StudentService {
 
-  public getStudents(): Observable<IStudent[]> {
-    return of(STUDENTS);
+  constructor(
+    private _store: Store<IAppState>,
+    private appComponent: AppComponent
+  ) {}
+
+  studentList
+  public getStudents(): Observable<any> {
+    // this._store.select('journal', 'students')
+    //   .subscribe(data => this.studentList = data);
+    // return of(this.studentList)
+    return of(this._store.pipe(select(selectStudentList)));
   }
 
-  public addStudent(student: IStudent): Observable<IStudent[]> {
-    student.id = STUDENTS.length + 1;
-    STUDENTS.push(student);
-    return of(STUDENTS);
+  public addStudent(student): void{
+    this._store.dispatch(new AddStudent(student));
+    this.appComponent.createComponent("Student successfuly added", "success");
   }
 
 }
