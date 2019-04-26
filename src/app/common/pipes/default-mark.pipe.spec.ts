442a112d0017313de3f60c250740ceed1ea52fb1
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-26 18:06:15 +0300

    feat: add test for defaul mark pipe

---
+++ src/app/common/pipes/default-mark.pipe.spec.ts
@@ -0,0 +1,15 @@
+import { DefaultMarkPipe } from "./default-mark.pipe";
+
+
+describe('DefaultMarkPipe', () => {
+  let pipe = new DefaultMarkPipe();
+
+  it('should transform "NaN" to "-"', () => {
+    expect(pipe.transform(NaN)).toBe('-');
+  })
+
+  it('should return value', () => {
+    expect(pipe.transform(10)).toBe(10);
+  })
+
+})
