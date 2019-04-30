63257a69c626b909b5ee61f1c9ccde97516ca0e3
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-30 18:16:03 +0300

    refac: remove unnecessary methods

--- src/app/common/services/subjects.service.ts
+++ src/app/common/services/subjects.service.ts
@@ -1,27 +1,10 @@
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 
-import { ISubject } from "../../common/entities";
-import { SUBJECTS } from "src/app/mock-subjects";
-
 @Injectable({
   providedIn: "root"
 })
 export class SubjectService {
-
-  public getSubjects(): Observable<ISubject[]> {
-    return of(SUBJECTS);
-  }
-
-  public addSubject(subject: ISubject): Observable<ISubject[]> {
-    subject.id = SUBJECTS.length + 1;
-    SUBJECTS.push(subject);
-    return of(SUBJECTS);
-  }
-
-  public getSubject(name: string): Observable<ISubject> {
-    return of(SUBJECTS.find(subject => subject.name === name));
-  }
 
   public getCurrentDate(): Observable<string> {
     let d: Date = new Date();
