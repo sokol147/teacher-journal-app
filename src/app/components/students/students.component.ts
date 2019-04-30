318b81a0d26cb931d0e08fb8b448429e5c91db7e
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-04-30 17:56:42 +0300

    feat: change select method to get data from store

--- src/app/components/students/students.component.ts
+++ src/app/components/students/students.component.ts
@@ -2,17 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { IStudent } from "../../common/entities";
 import { ButtonType, IButton } from "../../shared/components/button/button.model";
 
-// import { Store, select } from "@ngrx/store";
-// import { IAppState } from "../../store/state/app.state";
+import { Store, select } from "@ngrx/store";
+import { IAppState } from "../../store/state/app.state";
 
-// import { AddStudent } from "../../store/actions/app.actions";
-
-// import {AppComponent } from "../../root/app.component";
-// import { selectStudentList } from "src/app/store/selectors/student.selector";
-// import { Observable } from 'rxjs';
-// import { IStudentState } from 'src/app/store/state/student.state';
-import { StudentService } from 'src/app/common/services/students.service';
-import { IFormField } from 'src/app/shared/components/form/form.model';
+import { StudentService } from "src/app/common/services/students.service";
+import { IFormField } from "src/app/shared/components/form/form.model";
 
 @Component({
   selector: "app-students",
@@ -20,7 +14,7 @@ import { IFormField } from 'src/app/shared/components/form/form.model';
   styleUrls: ["./students.component.scss"],
   providers: [ StudentService ]
 })
-export class StudentsComponent implements OnInit{
+export class StudentsComponent implements OnInit {
 
   private button: IButton = {
     class: ButtonType.Add
@@ -32,29 +26,20 @@ export class StudentsComponent implements OnInit{
     { label: "Address", isRequired: false, id: "address" }
   ];
 
-  // public students$: Observable<IStudentState> = this._store.pipe(select(selectStudentList));
-
-  students: IStudent[];
+  public students: IStudent[];
 
   public path: string[] = ["students"];
   public order: number = -1;
 
   constructor(
-    // private _store: Store<IAppState>,
-    // private appComponent: AppComponent,
+    private _store: Store<IAppState>,
     private studentService: StudentService
   ) {}
 
-  ngOnInit(): void{
-    // this.getStudents()
-    this.studentService.getStudents()
-      .subscribe(data => this.students = data)
+  public ngOnInit(): void {
+    this._store.select("students", "students")
+      .subscribe(data => this.students = data);
   }
-
-  // public getStudents(): void{
-  //   this.studentService.getStudents()
-  //     .subscribe(data => this.students = data)
-  // }
 
   public addStudent(student: IStudent): void {
     let _student: IStudent = {
@@ -65,8 +50,6 @@ export class StudentsComponent implements OnInit{
       address: student.address,
     };
     this.studentService.addStudent(_student);
-    // this._store.dispatch(new AddStudent(_student));
-    // this.appComponent.createComponent("Student successfuly added", "success");
   }
 
   public sortTable(prop: string): boolean {
