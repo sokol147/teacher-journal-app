496df413dfa0f454300b07d6c4307b6051ac6e7f
Uladzislau Sakalou1 <Uladzislau_Sakalou1@epam.com>
2019-05-02 17:41:32 +0300

    feat: add export component to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { SubjectsComponent } from "./components/subjects/subjects.component";
 import { SubjectTableComponent } from "./components/subject-table/subject-table.component";
 import { StatisticComponent } from "./components/statistic/statistic.component";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { ExportComponent } from './components/export/export.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/students", pathMatch: "full"},
   { path: "students", component: StudentsComponent },
   { path: "subjects", component: SubjectsComponent },
+  { path: "export", component: ExportComponent},
   { path: "subject/:name", component: SubjectTableComponent },
   { path: "statistic", component: StatisticComponent },
   { path: "**", component: PageNotFoundComponent }
